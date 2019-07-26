ada2ea4982616be53730af318966b3b61eebba83
Jennfer Silva <jenny.ms@outlook.com.br>
2019-07-26 13:34:25 +0100

    Project set up

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+  
+    return (
+      <div>
+        code here
+      </div>
+    )
+  }
+  
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
